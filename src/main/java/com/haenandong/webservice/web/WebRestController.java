7b2bf65f1e49e975f596f5eccc488fe766e75f3c
youngran <horany83@gmail.com>
2019-08-21 13:09:26 +0900

    update

--- src/main/java/com/haenandong/webservice/web/WebRestController.java
+++ src/main/java/com/haenandong/webservice/web/WebRestController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.haenandong.webservice.domain.posts.PostsRepository;
 import com.haenandong.webservice.dto.posts.PostsSaveRequestDto;
+import com.haenandong.webservice.service.PostsService;
 
 import lombok.AllArgsConstructor;
 
@@ -14,16 +14,24 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class WebRestController {
 	
-	private PostsRepository postsRepository;
+	//기존소스
+//	private PostsRepository postsRepository;
+	private PostsService postsService;
 
 	@GetMapping("/hello")
 	public String hello() {
 		return "HelloWorld";
 	}
 	
+	//기존소스
+//	@PostMapping("/posts")
+//	public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+//		postsRepository.save(dto.toEntity());
+//	}
+	
 	@PostMapping("/posts")
-	public void savePosts(@RequestBody PostsSaveRequestDto dto) {
-		postsRepository.save(dto.toEntity());
+	public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
+		return postsService.save(dto);
 	}
 	
 }
