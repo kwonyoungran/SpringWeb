7b789cb143d5427ce03607996e6fa2a160c3651c
youngran <horany83@gmail.com>
2019-08-28 17:30:33 +0900

    update

--- src/main/java/com/haenandong/webservice/Application.java
+++ src/main/java/com/haenandong/webservice/Application.java
@@ -4,18 +4,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing	// JPA Auditing 활성화
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
 	
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
-			+ "/app/config/apringboot-webservice/real-application.yml";
+			+ "/app/config/springboot-webservice/real-application.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class)
-		.properties(APPLICATION_LOCATIONS)
-		.run(args);
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 //		SpringApplication.run(Application.class, args);
 		
 
